5f9a9bc3ae123d1bbf57566703c7c706d7741486
moonlight87 <coganglen1992@X>
2016-02-22 15:21:41 +0700

    v1-20160222-02

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
-$(document).ready(function (){
-	$.ajax({
-		url: "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/b0d1722b04b0a737aade2ce6e055263625a0b435/Common-Currency.json"
-	})
-	.done(function( jsonData ) {
-		var currencies = JSON.parse(jsonData);
+$(document).ready(function(){
+	$("button").click(function(){
+		$.ajax({
+			url: "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/b0d1722b04b0a737aade2ce6e055263625a0b435/Common-Currency.json",
+			dataType: "text",
+			success: function(data){
+				var currencies = JSON.parse(data);
 
-		for(var currencyKey in currencies){
-			var symbol = currencies[currencyKey].symbol;
-			var currencyElement = "<li><a href='#'>" + currencyKey + ' - ' + symbol + '</a></li>';
-			$(currencyElement).appendTo($('#ddl'));
-		}
+				for(var currencyKey in currencies){
+					var symbol = currencies[currencyKey].symbol;
+					var currencyElement = "<li><a href='#'>" + currencyKey + ' - ' + symbol + '</a></li>';
+					$(currencyElement).appendTo($("ul"));
+				}
+			}
+		});		
 	});
- 
-	var isDisplay = true;
-	$("#ddlButton").on("click", function(){
-  	if(isDisplay){
-    	$("#ddl").addClass("displayed");
-    } else{
-    	$("#ddl").removeClass("displayed");
-    }
-    isDisplay =! isDisplay;
-  });
 });
