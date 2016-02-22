6f278b2e5e2cae5fab25fa47b1ff15453115afdc
moonlight87 <coganglen1992@X>
2016-02-22 16:08:17 +0700

    v1-20160222-03

--- js/index.js
+++ js/index.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
 	$("button").click(function(){
 		$.ajax({
 			url: "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/b0d1722b04b0a737aade2ce6e055263625a0b435/Common-Currency.json",
-			dataType: "text",
+			type: "post",
+			dataType: "json",
 			success: function(data){
-				var currencies = JSON.parse(data);
-
-				for(var currencyKey in currencies){
-					var symbol = currencies[currencyKey].symbol;
+				for(var currencyKey in data){
+					var symbol = data[currencyKey].symbol;
 					var currencyElement = "<li><a href='#'>" + currencyKey + ' - ' + symbol + '</a></li>';
 					$(currencyElement).appendTo($("ul"));
 				}
